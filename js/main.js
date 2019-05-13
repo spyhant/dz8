98f601e92bbaf34d02e1d96a4b4f92258e4e5228
Alex <spyhant@gmail.com>
2019-05-13 05:14:51 +0300

    Моё дз 7

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+let knopka = document.getElementById("start");
+let bloks = document.querySelector('.result-table');
+let inp = document.querySelectorAll('.expenses-item');
+let butt = document.getElementsByTagName("button");
+let neobrasx = document.querySelectorAll('.optionalexpenses-item');
+let other = document.querySelector('.choose-income-label'),
+    chekbox = document.querySelector(".checksavings"),
+    sum = document.querySelector('.choose-sum'),
+    percent = document.querySelector('.choose-percent'),
+    year = document.querySelector('.year-value'),
+    month = document.querySelector('.month-value'),
+    day = document.querySelector('.day-value');
